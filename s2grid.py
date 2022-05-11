b092d1d42bfffe2c11ccdc1c297598431cdb8bc4
yuvalsolaz <yuval.solaz@gmail.com>
2022-05-11 19:00:47 +0300

    geo2cell

--- s2grid.py
+++ s2grid.py
@@ -11,10 +11,37 @@ system.  Such systems work by converting documents into a collection of
 "index terms" (e.g., representing words or phrases), and then building an
 "inverted index" that maps each term to a list of documents (and document
 positions) where that term occurs.
+
 This example shows how to convert spatial data into index terms, which can
-then be indexed along with the other document information.
+then be indexed along with other document information.
 This is a port of the C++ term_index.cc example for the Python API.
 """
 
-def geo2label(geo, level):
-    pass
+'''
+    Usage Example:
+    london = s2.S2LatLngRect(s2.S2LatLng.FromDegrees(51.3368602, 0.4931979),
+                         s2.S2LatLng.FromDegrees(51.7323965, 0.1495211))
+    e14lj = s2.S2LatLngRect(s2.S2LatLng.FromDegrees(51.5213527, -0.0476026),
+                    s2.S2LatLng.FromDegrees(51.5213527, -0.0476026))
+    print(f'london contains e14lj: {london.Contains(e14lj)}')
+     
+'''
+
+import s2geometry as s2
+
+def geo2cell(lat, lon, level):
+    p = s2.S2LatLng.FromDegrees(lat,lon)
+    leaf = s2.S2CellId(p)
+    cell = leaf.parent(level)
+    return cell
+
+import sys
+if __name__ == '__main__':
+    level = 4
+    lat = 51.3368602
+    lon = 0.4931979
+    if len(sys.argv) > 1:
+        level = int(sys.argv[1])
+    cell = geo2cell(lat=lat, lon=lon, level=level)
+    print(f'lat:{lat} lon:{lon} level:{level} area:{s2.S2Cell(cell).ExactArea()}')
+    print(f'label:{cell.id()} {cell.ToToken()} {cell.ToString()}')
