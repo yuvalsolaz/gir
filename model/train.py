6e0267b008fcab19dc622525aafe5f31a1f66584
yuval solaz <yuval.solaz@gmail.com>
2022-05-29 09:58:34 +0300

    batchsize 32

--- model/train.py
+++ model/train.py
@@ -81,8 +81,8 @@ def train(dataset_path, checkpoint):
 
     training_args = TrainingArguments(output_dir='trainer',
                                       report_to=['tensorboard'],
-                                      per_device_train_batch_size=16,
-                                      per_device_eval_batch_size=16,
+                                      per_device_train_batch_size=32,
+                                      per_device_eval_batch_size=32,
                                       num_train_epochs=20,
                                       logging_steps=50,
                                       save_steps=500,
