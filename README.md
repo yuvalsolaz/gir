50c400f0e885c94d35c4ec6a59d17536d91a9121
yuval solaz <yuval.solaz@gmail.com>
2022-06-15 15:49:47 +0300

    vitalush

--- README.md
+++ README.md
@@ -3,3 +3,9 @@
 Implementation for geocoding model 
 
 
+TODO: 
+1. label 4 cube sides (discard poles)
+4. validation benchmark  
+5. geo visualization for inference 
+6. train on tweeter data
+7. beam search for output sequence 
--- model/test.py
+++ model/test.py
@@ -1,33 +1,8 @@
-'''
-  Args:
-        task (`str`):
-            The task defining which pipeline will be returned. Currently accepted tasks are:
-
-            - `"audio-classification"`
-            - `"automatic-speech-recognition"`
-            - `"conversational"`
-            - `"feature-extraction"`
-            - `"fill-mask"`
-            - `"image-classification"`
-            - `"question-answering"`
-            - `"table-question-answering"`
-            - `"text2text-generation"`
-            - `"text-classification"` (alias `"sentiment-analysis"` available)
-            - `"text-generation"`
-            - `"token-classification"` (alias `"ner"` available)
-            - `"translation"`
-            - `"translation_xx_to_yy"`
-            - `"summarization"`
-            - `"zero-shot-classification"`
-            - `"zero-shot-image-classification"`
-'''
-
 import sys
 import numpy as np
 import pandas as pd
 from transformers import AutoModelForSeq2SeqLM
 from transformers import AutoTokenizer
-from transformers import pipeline
 
 if __name__ == '__main__':
 
@@ -46,14 +21,21 @@ if __name__ == '__main__':
     params = sum([np.prod(p.size()) for p in model_parameters])
     print(f'model loaded with {params} parameters')
 
-    generator = pipeline(task='summarization', model=model, tokenizer=tokenizer)
     df = pd.read_csv(samples_file)
     if 'input' not in df.columns:
         print(f'invalid sample file: missing input column in {samples_file} file')
         print(df.head())
         exit()
 
+    def inference(sentence):
+        input_ids = tokenizer(sentence, return_tensors="pt").input_ids
+        outputs = model.generate(input_ids)
+        return tokenizer.batch_decode(outputs, skip_special_tokens=True)
+
     print(f'evaluates {df.shape[0]} samples from {samples_file}')
-    df['inference'] = df.apply(lambda t : generator(t['input']), axis=1)
+    df['inference'] = df.apply(lambda t : inference(t['input']), axis=1)
     print(df[['input','inference']].head())
+    output_file = samples_file.replace('.csv', '_infer.csv')
+    print(f'save inference results to {output_file}')
+    df.to_csv(output_file)
 
