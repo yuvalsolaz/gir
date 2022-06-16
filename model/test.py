3fb063bb6807544cedf2277ef21fd8b80d1eacad
yuvalsolaz <yuval.solaz@gmail.com>
2022-06-16 14:57:49 +0300

    accuracy

--- model/test.py
+++ model/test.py
@@ -1,9 +1,11 @@
 import sys
 import numpy as np
 import pandas as pd
+from sklearn.metrics import accuracy_score, confusion_matrix, ConfusionMatrixDisplay
 from transformers import AutoModelForSeq2SeqLM
 from transformers import AutoTokenizer
 from tqdm import tqdm
+import matplotlib.pyplot as plt
 
 if __name__ == '__main__':
 
@@ -22,7 +24,7 @@ if __name__ == '__main__':
     params = sum([np.prod(p.size()) for p in model_parameters])
     print(f'model loaded with {params} parameters')
 
-    df = pd.read_csv(samples_file)[:1000]
+    df = pd.read_csv(samples_file,dtype=str)
     if 'input' not in df.columns:
         print(f'invalid sample file: missing input column in {samples_file} file')
         print(df.head())
@@ -44,12 +46,27 @@ if __name__ == '__main__':
     # df['inference'] = df.apply(lambda t : inference(t['input']), axis=1)
     rows = []
     for index, row in tqdm(df.iterrows(), total=df.shape[0]):
-        row['inference'] = inference(row['input'])
-        rows.append(row)
+        if row['s2_sequence'] == row['s2_sequence']:
+            row['inference'] = inference(row['input'])[0]
+            if row['inference'] and row['inference'] != np.nan:
+                rows.append(row)
 
     out_df = pd.DataFrame(rows)
+    print(f'{out_df.shape[0]} inference rows out of {df.shape[0]} samples')
 
     print(out_df[['input', 'inference']].head())
+
+    accuracy = accuracy_score(y_true=out_df['s2_sequence'], y_pred=out_df['inference'])
+    print(f'accuracy: {accuracy}')
+    cm = confusion_matrix(y_true=out_df['s2_sequence'], y_pred=out_df['inference'], normalize='true')
+    # cmd = ConfusionMatrixDisplay(cm, display_labels=np.append(out_df['inference'].unique(),'un'))
+    cmd = ConfusionMatrixDisplay(cm[:10,:10], display_labels=out_df['inference'].unique()[:10])
+    plt.rcParams["figure.figsize"] = (10, 10)
+    cmd.plot()
+    plt.show()
+
     output_file = samples_file.replace('.csv', '_infer.csv')
     print(f'save inference results to {output_file}')
     out_df.to_csv(output_file)
+
+
