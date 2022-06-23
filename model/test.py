20301e9f08460a4ebb8d7db77e4b95f7e9a2a0ac
yuval solaz <yuval.solaz@gmail.com>
2022-06-23 19:08:07 +0300

    load test data

--- model/test.py
+++ model/test.py
@@ -4,16 +4,22 @@ import pandas as pd
 from sklearn.metrics import accuracy_score, confusion_matrix, ConfusionMatrixDisplay
 from transformers import AutoModelForSeq2SeqLM
 from transformers import AutoTokenizer
+import datasets
 from tqdm import tqdm
 import matplotlib.pyplot as plt
+
 
 if __name__ == '__main__':
 
     if len(sys.argv) < 2:
-        print(f'usage: python {sys.argv[0]} <model file> <samples file>')
+        print(f'usage: python {sys.argv[0]} <model file> <dataset_path>')
         exit(1)
     checkpoint = sys.argv[1]
-    samples_file = sys.argv[2]
+    dataset_path = sys.argv[2]
+
+    print(f'load dataset from: {dataset_path}')
+    ds = datasets.load_from_disk(dataset_path=dataset_path)
+    test = ds['test'].select(range(1000))
 
     print(f'loading tokenizer from t5-small')
     tokenizer = AutoTokenizer.from_pretrained('t5-small')
@@ -24,13 +30,6 @@ if __name__ == '__main__':
     params = sum([np.prod(p.size()) for p in model_parameters])
     print(f'model loaded with {params} parameters')
 
-    df = pd.read_csv(samples_file,dtype=str)
-    if 'input' not in df.columns:
-        print(f'invalid sample file: missing input column in {samples_file} file')
-        print(df.head())
-        exit()
-
-
     def inference(sentence):
         input_ids = tokenizer(sentence, return_tensors="pt").input_ids
         outputs = model.generate(input_ids,
@@ -38,35 +37,18 @@ if __name__ == '__main__':
                                  num_beams=10,
                                  length_penalty=0.0,
                                  output_scores=True)
-        return tokenizer.batch_decode(outputs, skip_special_tokens=True)
+        return tokenizer.batch_decode(outputs, skip_special_tokens=True)[0]
 
+    print(f'evaluates {test.shape[0]} samples from {dataset_path}')
 
-    print(f'evaluates {df.shape[0]} samples from {samples_file}')
+    test = test.map(lambda x: {'inference': inference(x["english_desc"]) if x["english_desc"] is not None else ''})
+    accuracy = accuracy_score(y_true=test['s2_sequence'], y_pred=test['inference'])
+    print(f'\n accuracy: {accuracy}')
+    # save to disk
+    output_path = 'inference.csv'
+    print(f'save dataset to: {output_path}')
+    test.set_format('pandas')
+    test.to_csv(output_path)
 
-    # df['inference'] = df.apply(lambda t : inference(t['input']), axis=1)
-    rows = []
-    for index, row in tqdm(df.iterrows(), total=df.shape[0]):
-        if row['s2_sequence'] == row['s2_sequence']:
-            row['inference'] = inference(row['input'])[0]
-            if row['inference'] and row['inference'] != np.nan:
-                rows.append(row)
-
-    out_df = pd.DataFrame(rows)
-    print(f'{out_df.shape[0]} inference rows out of {df.shape[0]} samples')
-
-    print(out_df[['input', 'inference']].head())
-
-    accuracy = accuracy_score(y_true=out_df['s2_sequence'], y_pred=out_df['inference'])
-    print(f'accuracy: {accuracy}')
-    cm = confusion_matrix(y_true=out_df['s2_sequence'], y_pred=out_df['inference'], normalize='true')
-    # cmd = ConfusionMatrixDisplay(cm, display_labels=np.append(out_df['inference'].unique(),'un'))
-    cmd = ConfusionMatrixDisplay(cm[:10,:10], display_labels=out_df['inference'].unique()[:10])
-    plt.rcParams["figure.figsize"] = (10, 10)
-    cmd.plot()
-    plt.show()
-
-    output_file = samples_file.replace('.csv', '_infer.csv')
-    print(f'save inference results to {output_file}')
-    out_df.to_csv(output_file)
 
 
