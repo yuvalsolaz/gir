07cafb986ba9068fb29004e822a6bb1c189d069e
yuvalsolaz <yuval.solaz@gmail.com>
2022-07-13 18:49:53 +0300

    hierarchy metrics

--- model/test.py
+++ model/test.py
@@ -1,12 +1,43 @@
 import sys
 import numpy as np
-import pandas as pd
 from sklearn.metrics import accuracy_score, confusion_matrix, ConfusionMatrixDisplay
 from transformers import AutoModelForSeq2SeqLM
 from transformers import AutoTokenizer
 import datasets
-from tqdm import tqdm
-import matplotlib.pyplot as plt
+
+'''
+Pi predicted class for each test example i, and all of its ancestor classes (car, pickup, isuzu)    
+Ti true class of test example i, and all its ancestor classes
+
+Definitions for hierarchical precision hP
+    hP(predict,) = sum(Pi intersect Ti) / sum(Pi)  
+
+Definitions for hierarchical recall hR
+    hR(predict,) = sum(Pi intersect Ti) / sum(Ti)  
+
+Definitions for hierarchical f-measure hF
+    hF = 2 * hP * hR / (hP + hR)
+'''
+
+def hirarchies_metric(y_true, y_pred):
+
+    def intersect(true_seq, pred_seq):
+        for i in range(min(len(true_seq),len(pred_seq))):
+            if true_seq[i] != pred_seq[i]:
+                break
+        return true_seq[:i+1]
+
+    x_array = np.array(list(zip(y_true,y_pred)))
+
+    hA_seq = [intersect(sample[0], sample[1]) for sample in x_array]
+
+    vlen = np.vectorize(len)
+    Ti = vlen(x_array[0])
+    hA = vlen(hA_seq)
+    haccuracy = np.divide(hA, sum(Ti))
+    return haccuracy
+
+
 
 
 if __name__ == '__main__':
@@ -19,7 +50,7 @@ if __name__ == '__main__':
 
     print(f'load dataset from: {dataset_path}')
     ds = datasets.load_from_disk(dataset_path=dataset_path)
-    test = ds['train'].select(range(1000))
+    test = ds['train'].select(range(10))
 
     print(f'loading tokenizer from t5-base')
     tokenizer = AutoTokenizer.from_pretrained('t5-base')
@@ -39,13 +70,15 @@ if __name__ == '__main__':
                                  output_scores=True,
                                  return_dict_in_generate=True
                                  )
-        return tokenizer.batch_decode(outputs, skip_special_tokens=True)[0]
+        end_of_sequence = '\x00'
+        return tokenizer.batch_decode(outputs['sequences'], skip_special_tokens=True)[0] + end_of_sequence
 
     print(f'evaluates {test.shape[0]} samples from {dataset_path}')
 
     test = test.map(lambda x: {'inference': inference(x["english_desc"]) if x["english_desc"] is not None else ''})
     accuracy = accuracy_score(y_true=test['s2_sequence'], y_pred=test['inference'])
-    print(f'\n accuracy: {accuracy}')
+    h_accuracy = hirarchies_metric(y_true=test['s2_sequence'], y_pred=test['inference'])
+    print(f'\n accuracy: {accuracy} \n hierarchy accuracy: {h_accuracy}')
     # save to disk
     output_path = 'inference.csv'
     print(f'save dataset to: {output_path}')
