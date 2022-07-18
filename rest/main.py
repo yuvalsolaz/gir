e6a48edf6c4b5d2552bc9359b42b12f0e1f92284
yuval solaz <yuval.solaz@gmail.com>
2022-07-18 17:39:59 +0300

    bugs

--- rest/main.py
+++ rest/main.py
@@ -1,22 +1,22 @@
 from typing import Union, List
 from fastapi import FastAPI, HTTPException
 from fastapi.middleware.cors import CORSMiddleware
+import uvicorn
 from pydantic import BaseModel
 
-class Point(BaseModel):
-    lat: float = 0.0
-    lon: float = 0.0
-
-
-class BBoxDegrees(BaseModel):
-    bbox: List[Point]
-
-
 app = FastAPI()
-app.add_middleware(CORSMiddleware,allow_origins=["*"], # Allows all origins
-                                  allow_credentials=True,
-                                  allow_methods=["*"], # Allows all methods
-                                  allow_headers=["*"]) # Allows all headers
+app.add_middleware(CORSMiddleware, allow_origins=["*"],  # Allows all origins
+                   allow_credentials=True,
+                   allow_methods=["*"],  # Allows all methods
+                   allow_headers=["*"])  # Allows all headers
+
+
+class GeocodeResults(BaseModel):
+    display_name: str
+    confidance: float
+    boundingbox: List[float]
+    lat: float
+    lon: float
 
 
 @app.get("/")
@@ -24,20 +24,21 @@ def read_root():
     return {"Usage": "/geocoding/<text>"}
 
 
-@app.get("/geocoding/{text}", response_model=BBoxDegrees)
+@app.get("/geocoding", response_model=List[GeocodeResults])
 def geocoding(text: str):
-    # TODO: model inference on text:
-    _bbox = [Point(lat=34.5, lon=31.8),
-            Point(lat=34.6, lon=31.8),
-            Point(lat=34.6, lon=31.7),
-            Point(lat=34.5, lon=31.7),
-            Point(lat=34.5, lon=31.8)]
 
-    res = BBoxDegrees(bbox=_bbox)
+    # TODO: model inference on text:
+    res = GeocodeResults(display_name= 'language model',
+                         confidance = 0.76,
+                         boundingbox = [34.5,34.6, 31.8, 31.7],
+                         lat = 34.55,
+                         lon = 31.75)
 
     if res is None:
         raise HTTPException(status_code=404, detail="geocoding error")
 
-    print(res)
+    return [res]
 
-    return res
+
+if __name__ == '__main__':
+    uvicorn.run(app, host='localhost', port=5000)
