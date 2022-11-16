d96b6e2658c41e742260b8fd32bcab3bb0811a2f
yuvalsolaz <yuval.solaz@gmail.com>
2022-11-16 12:17:25 +0200

    litshufim

--- model/evaluation.py
+++ model/evaluation.py
@@ -1,6 +1,7 @@
 import sys
 import pandas as pd
 from shapely import geometry
+import shapely.wkt as wkt
 import pyproj
 from tqdm import tqdm
 from model.inference import load_model, inference
@@ -8,13 +9,14 @@ from model.geolabel import get_token_polygon
 
 transform = pyproj.Transformer.from_crs("epsg:4326", "epsg:32636")
 
+
 def geo2utm(geo_coord_list):
     utm_coord_list = list()
     for coord in geo_coord_list:
         lat = coord[0]
         lon = coord[1]
         x, y = transform.transform(lat, lon)
-        utm_coord_list.append((x,y))
+        utm_coord_list.append((x, y))
     return utm_coord_list
 
 
@@ -29,18 +31,6 @@ def get_sentence_polygon(text):
     utm_inference_poly = geo2utm(inference_poly)
     return geometry.Polygon(utm_inference_poly).wkt
 
-#
-# def get_label_distance(gt_location, text):
-#     inference_poly = get_sentence_polygon(text)
-#     utm_inference_poly = geo2utm(inference_poly)
-#     utm_gt_location = geo2utm([gt_location])
-#     sh_utm_inference_poly = geometry.Polygon(utm_inference_poly)
-#     sh_utm_gt_location = geometry.Point(utm_gt_location)
-#     distance = sh_utm_inference_poly.distance(sh_utm_gt_location)
-#     centroid_distance = sh_utm_inference_poly.centroid.distance(sh_utm_gt_location)
-#     print(f'distance:{distance} centroid distance:{centroid_distance} text length:{len(text)}- {text[:100]}')
-#     return centroid_distance
-
 
 if __name__ == '__main__':
     if len(sys.argv) < 3:
@@ -50,28 +40,32 @@ if __name__ == '__main__':
     checkpoint = sys.argv[2]
 
     print(f'loading {evaluation_file}...')
-    df = pd.read_json(evaluation_file)
+    df = pd.read_json(evaluation_file)[:10]
     print(f'{df.shape[0]} records loaded')
 
     tokenizer, model = load_model(checkpoint=checkpoint)
     tqdm.pandas()
 
+    output_file = evaluation_file.replace('.json', '_inference.csv')
+    print('inference polygons....')
     df['inference_polygon'] = df.progress_apply(lambda t: get_sentence_polygon(t['text']), axis=1)
-    df['gt_point'] = df.progress_apply(lambda t: geo2utm([[t['lat'],t['lon']]]), axis=1)
+    print(f'write {df.shape[0]} records with inference polygon to {output_file}')
+    df.to_csv(output_file)
 
-    def distance(t):
-        return geometry.Polygon(t['inference_polygon']).distance(t['gt_location'])
-
-    def centroid_distance(t):
-        return geometry.Polygon(t['inference_polygon']).centroid.distance(t['gt_location'])
-
-    df['distance'] = df.apply(lambda t: distance(t), axis=1)
-    df['centrid_distance'] = df.apply(lambda t: centroid_distance(t), axis=1)
-
-    output_file = evaluation_file.replace('.json','_inference.csv')
-    print(f'write {df.shape[0]} records with error distances to {output_file}')
+    print('convert gt locations from geo to utm....')
+    df['gt_location'] = df.progress_apply(lambda t: geometry.Point(geo2utm([[t['lat'], t['lon']]])).wkt, axis=1)
+    print(f'write {df.shape[0]} records with utm locations to {output_file}')
     df.to_csv(output_file)
 
 
+    def get_distance(t, use_centroid=False):
+        infer_polygon = wkt.loads(t['inference_polygon'])
+        gt_loc = wkt.loads(t['gt_location'])
+        return infer_polygon.centroid.distance(gt_loc) if use_centroid else infer_polygon.distance(gt_loc)
 
 
+    print('calculates distance and centroid distance from inference polygon to gt location....')
+    df['distance'] = df.apply(lambda t: get_distance(t, use_centroid=False), axis=1)
+    df['centrid_distance'] = df.apply(lambda t: get_distance(t, use_centroid=True), axis=1)
+    print(f'write {df.shape[0]} records with distances to {output_file}')
+    df.to_csv(output_file)
