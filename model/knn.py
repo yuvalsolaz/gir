ee727b84a7a09d4220ec46d6a78cd18b53250645
Yuval <yuval@Yuvals-MacBook-Pro.local>
2023-07-11 15:22:45 +0300

    knn search module

---
+++ model/knn.py
@@ -0,0 +1,61 @@
+import sys
+import torch
+import datasets
+
+from transformers import AutoTokenizer, AutoModel
+
+device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+
+
+class SearchEngine(object):
+
+    def __init__(self, dataset_path, model_checkpoint):
+        print(f'load dataset from {dataset_path}')
+        self.ds = datasets.load_from_disk(dataset_path=dataset_path)
+
+        print(f'load model from {model_checkpoint}')
+        self.tokenizer = AutoTokenizer.from_pretrained(model_checkpoint)
+        self.model = AutoModel.from_pretrained(model_checkpoint)
+        self.model.to(device)
+
+        print(f'calculates embeddings for the dataset')
+
+    @staticmethod
+    def cls_pooling(model_output):
+        return model_output.last_hidden_state[:, 0]
+
+    @staticmethod
+    # Mean Pooling - Take attention mask into account for correct averaging
+    def mean_pooling(model_output, attention_mask):
+        token_embeddings = model_output[0]  # First element of model_output contains all token embeddings
+        input_mask_expanded = attention_mask.unsqueeze(-1).expand(token_embeddings.size()).float()
+        return torch.sum(token_embeddings * input_mask_expanded, 1) / torch.clamp(input_mask_expanded.sum(1), min=1e-9)
+
+    def get_embeddings(self, text: str):
+        encoded_input = self.tokenizer(
+            text, padding=True, truncation=True, return_tensors="pt"
+        )
+        encoded_input = {k: v.to(device) for k, v in encoded_input.items()}
+        model_output = model(**encoded_input)
+        return SearchEngine.cls_pooling(model_output)
+
+    def search(self, query: str, k=5):
+        print(f'calculate embeddings for query text')
+
+        print(f'get {k} nearest samples from dataset')
+
+
+if __name__ == '__main__':
+    if len(sys.argv) < 2:
+        print(f'usage: python {sys.argv[0]} <dataset path> <model checkpoint>')
+        exit(1)
+    dataset_path = sys.argv[1]
+    model_checkpoint = sys.argv[2]
+    search_engine = SearchEngine(dataset_path=dataset_path, model_checkpoint=model_checkpoint)
+
+    while True:
+        query = input('type search query:')
+        res = search_engine.search(query, k=5)
+        print(f'{query} : {res}')
+
+    search_engine.search()
